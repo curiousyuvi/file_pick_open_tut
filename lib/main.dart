417734939928c2b585f9cc8fe17a8a90011debd7
Yuvraj Singh <ysgaur9919@gmail.com>
2021-11-19 17:27:18 +0530

    created basic ui

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          child: MaterialButton(
+            onPressed: () {},
+            child: Text(
+              'Pick and open file',
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.green,
+          ),
+        ),
+      ),
     );
   }
 }
